677ab3547f497761eb904bdc347e68568992047e
Dalton <Lima>
2019-08-04 22:21:34 +0200

    18 - Blending

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -13,6 +13,9 @@
 #include "Shader.h"
 #include "Texture.h"
 
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+
 int main(void)
 {
 	GLFWwindow* window;
@@ -76,9 +79,13 @@ int main(void)
 		// index buffer object
 		IndexBuffer ib(indices, 6);
 
+		glm::mat4 proj = glm::ortho(-2.0f, 2.0f, -1.5f, 1.5f, -1.0f, 1.0f);
+		//glm::mat4 perspective = glm::perspective(glm::radians(45.0f), 4.0f / 3.0f, -1.0f, 100.f);
+
 		Shader shader("res/shaders/Basic.shader");
 		shader.Bind();
 		shader.SetUniform4f("u_Color", 0.3f, 1.0f, 0.4f, 1.0f);
+		shader.SetUniformMat4f("u_MVP", proj);
 
 		Texture texture("res/textures/falloutboy.png");
 		texture.Bind();
