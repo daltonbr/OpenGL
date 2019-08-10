4646fb9d11ecad23f4b179b246f97429d4aab03e
Dalton <Lima>
2019-08-10 21:49:24 +0200

    24 - Setting up a test framework

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -20,6 +20,8 @@
 #include "imgui/imgui_impl_glfw.h"
 #include "imgui/imgui_impl_opengl3.h"
 
+#include "tests/TestClearColor.h"
+
 int main(void)
 {
 	GLFWwindow* window;
@@ -69,49 +71,8 @@ int main(void)
 
 	// Creating a scope here to keep a Valid OpenGL Context
 	{
-		float positions[] = {
-			-50.0f, -50.0f, 0.0f, 0.0f, // 0
-			 50.0f, -50.0f, 1.0f, 0.0f, // 1
-			 50.0f,  50.0f, 1.0f, 1.0f, // 2
-			-50.0f,  50.0f, 0.0f, 1.0f  // 3
-		};
-
-		// Index Buffer
-		unsigned int indices[] = {
-			0, 1, 2,
-			2, 3, 0
-		};
-
 		GLCall(glEnable(GL_BLEND));
 		GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));		
-
-		VertexArray va;
-		VertexBuffer vb(positions, 4 * 4 * sizeof(float));
-
-		VertexBufferLayout layout;
-		layout.Push<float>(2);
-		layout.Push<float>(2);
-		va.AddBuffer(vb, layout);
-
-		// index buffer object
-		IndexBuffer ib(indices, 6);
-
-		glm::mat4 proj = glm::ortho(0.f, 1280.f, 0.f, 720.f, -1.0f, 1.0f);
-		// creates a Identity matrix and translate it to simulate a camera movement
-		glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(0.f, 0.f, 0.f));  
-
-		Shader shader("res/shaders/Basic.shader");
-		shader.Bind();
-		shader.SetUniform4f("u_Color", 0.3f, 1.0f, 0.4f, 1.0f);
-
-		Texture texture("res/textures/falloutboy.png");
-		texture.Bind();
-		shader.SetUniform1i("u_Texture", 0);
-
-		va.Unbind();
-		vb.Unbind();
-		ib.Unbind();
-		shader.Unbind();
 
 		Renderer renderer;
 
@@ -130,56 +91,21 @@ int main(void)
 		ImGui_ImplGlfw_InitForOpenGL(window, true);
 		ImGui_ImplOpenGL3_Init(glsl_version);
 
-		glm::vec3 translationA(200, 200, 0);
-		glm::vec3 translationB(400, 200, 0);
-
-		float red = 0.0f;
-		float increment = 0.01f;
-
-		/* Loop until the user closes the window */
+		test::TestClearColor test;
+		
 		while (!glfwWindowShouldClose(window))
 		{
-			/* Render here */
 			renderer.Clear();
+
+			test.OnUpdate(0.0f);
+			test.OnRender();
 
 			// Start the Dear ImGui frame
 			ImGui_ImplOpenGL3_NewFrame();
 			ImGui_ImplGlfw_NewFrame();
 			ImGui::NewFrame();
-
-			{
-				glm::mat4 model = glm::translate(glm::mat4(1.0f), translationA);
-				glm::mat4 mvp = proj * view * model;
-				//shader.SetUniform4f("u_Color", red, 0.5f, 0.4f, 1.0f);
-				shader.Bind();
-				shader.SetUniformMat4f("u_MVP", mvp);
-
-				renderer.Draw(va, ib, shader);
-			}
-
-			{
-				glm::mat4 model = glm::translate(glm::mat4(1.0f), translationB);
-				glm::mat4 mvp = proj * view * model;
-				shader.Bind();
-				shader.SetUniformMat4f("u_MVP", mvp);
-
-				renderer.Draw(va, ib, shader);
-			}
-
-			if (red > 1.0f)
-				increment = -0.01f;
-			else if (red < 0.0f)
-				increment = +0.01f;
-
-			red += increment;
-
-			// Show a simple window that we create ourselves. We use a Begin/End pair to created a named window.
-			{
-				ImGui::SliderFloat3("Translation A", &translationA.x, 0.0f, 1280.0f);
-				ImGui::SliderFloat3("Translation B", &translationB.x, 0.0f, 1280.0f);
-				ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
-			}
-
+			test.OnImGuiRender();
+			
 			// Rendering
 			ImGui::Render();
 			//int display_w, display_h;
@@ -189,10 +115,7 @@ int main(void)
 			//glClear(GL_COLOR_BUFFER_BIT);
 			ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
-			/* Swap front and back buffers */
 			GLCall(glfwSwapBuffers(window));
-
-			/* Poll for and process events */
 			GLCall(glfwPollEvents());
 		}
 	}
